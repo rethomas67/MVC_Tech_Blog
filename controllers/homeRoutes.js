6480778bdc37654db9652684d394d20748027b02
rthomas86 <rebkthomas72@gmail.com>
2024-05-13 16:54:08 -0700

    add routes for signup, login, and logout

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -26,4 +26,24 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/login", (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
+});
+
+router.get("/signup", (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("signup");
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
   });
