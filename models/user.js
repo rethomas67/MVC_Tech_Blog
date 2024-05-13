45b3b6828d7fb5468833ba3cce7f977ef74e9e48
rthomas86 <rebkthomas72@gmail.com>
2024-05-13 10:43:36 -0700

    created first controller

--- models/user.js
+++ models/user.js
@@ -19,15 +19,11 @@ User.init(
     name: {
       type: DataTypes.STRING,
       allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
       validate: {
-        isEmail: true,
+        len: [5],
       },
     },
+
     password: {
       type: DataTypes.STRING,
       allowNull: false,
